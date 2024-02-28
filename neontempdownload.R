806afcce818e6f39201376893b0215bc53ef3d86
ibbu543 <120696122+ibbu543@users.noreply.github.com>
2024-02-28 10:41:51 -0500

    Create neontempdownload.R
    added neon download function

---
+++ neontempdownload.R
@@ -0,0 +1,8 @@
+install.packages("neonUtilities")
+library(neonUtilities)
+
+temp <- function(d) { #date input as YYYY-MM-DD, same as Sys.Date() gives
+  download <- loadByProduct(dpID = "DP1.00003.001", site = "HARV", startdate = "2018-01", enddate = substr(Sys.Date(),0,7), tabl = "TAAT_30min", check.size = "F")
+  dat <- download$TAAT_30min
+  return(dat)
+}
