53f9e0d9142cf4454572187a501896aed57561e6
Katherine <krein21@bu.edu>
2024-02-28 13:10:26 -0500

    updated main to include temp data organized main using comments created a variable for todays date for more widespread us

--- Main.R
+++ Main.R
@@ -3,8 +3,30 @@ library(lubridate)
 install.packages("rMR")
 library(rMR)
 
+## Data downloads
+
+TODAYS_DATE = Sys.Date()
+
+## LAI and FPAR
+
 if(file.exists("01_LAI_FPAR_dwn.R"))      
   source("01_LAI_FPAR_dwn.R")
 
-lai_download(Sys.Date()-30)
-fpar_download(Sys.Date()-30)
+lai_download(TODAYS_DATE-30)
+fpar_download(TODAYS_DATE-30)
+
+## Temperature
+
+if(file.exists("neontempdownload.R"))
+  source("neontempdownload.R")
+
+temp_data = temp_download(TODAYS_DATE)
+
+# Save data to a file
+temp_file = paste("NEON.Temp.","2018-01.",TODAYS_DATE,".HARV.RData", sep='')
+file_path = file.path(getwd(), temp_file)
+
+if(file.exists(file_path)){
+}else{
+save(subset,file=file_path)
+}
