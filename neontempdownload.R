56323ef3008b5c176ee8cf362db34d6144ee7378
Cal <acrennaa@bu.edu>
2024-03-20 12:02:46 -0400

    I added a for loop to check if the file already exists before the download to match the code for the LAI/FPAR download functions

--- neontempdownload.R
+++ neontempdownload.R
@@ -2,7 +2,12 @@ install.packages("neonUtilities")
 library(neonUtilities)
 
 temp_download <- function(d) { #date input as YYYY-MM-DD, same as Sys.Date() gives
-  download <- loadByProduct(dpID = "DP1.00003.001", site = "HARV", startdate = "2018-01", enddate = substr(Sys.Date(),0,7), tabl = "TAAT_30min", check.size = "F")
+  temp_file <- paste("NEON.Temp.","2018-01.",substr(d,0,7),".HARV.RData", sep='')
+  file_path <- file.path(getwd(), temp_file)
+  if(file.exists(file_path)){print(file_path,'already exists')}
+  else{
+  download <- loadByProduct(dpID = "DP1.00003.001", site = "HARV", startdate = "2018-01", enddate = substr(d,0,7), tabl = "TAAT_30min", check.size = "F")
   dat <- download$TAAT_30min
+  }
   return(dat)
 }
