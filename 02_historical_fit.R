74888dd5c6123975abfd66e8f8b5328a965af098
Cal <acrennaa@bu.edu>
2024-04-18 11:30:25 -0400

    Changed data to NOAA, created new combined dataset

--- 02_historical_fit.R
+++ 02_historical_fit.R
@@ -106,27 +106,32 @@ ci <- apply(out[,x.cols],2,quantile,c(0.025,0.5,0.975))
 
 ####====================Amber's code====================
 
-if(file.exists("neontempdownload.R"))
-  source("neontempdownload.R")
+if(file.exists("01_NOAA_dwn.R"))
+  source("01_NOAA_dwn.R")
 
-temp_data = temp_download(Sys.Date())
+temp_data = noaa_historical_download("HARV","air_temperature",Sys.Date()-365) # dated this way in order to get all data a year before today, this can be changed
 
-temp = temp_data[c('startDateTime', 'tempTripleMean', 'tempTripleVariance')] |> rename(
-  datetime=startDateTime,
-  temp=tempTripleMean,
-  var=tempTripleVariance
-)
-temp$datetime = lubridate::as_datetime(temp$datetime)
+## the following is leftover from when we were using EFI temp data
+# temp_data = temp_data[c('mean_prediction', 'sd_prediction')] |> rename(
+#   temp=mean_prediction,
+#   temp_sd=sd_prediction
+# )
+# temp_data$datetime <- lubridate::as_datetime(temp_data$datetime)
+
+# let's create a dataset that includes all of our variables that we can call it when fitting our model with JAGS
+# this first run we'll be using the NOAA temperature data, which has hourly resolution, while the NEE data has half hourly resolution. For now, we'll match each half hour measurement to the corresponding hourly temperature
+temp_half_hour <- list(datetime=as.POSIXlt(seq(from=as.POSIXct(temp_data$datetime[1]),to=as.POSIXct(temp_data$datetime[length(temp_data$datetime)]), by = "30 min")),temp=rep(temp_data$mean_prediction,each=2)[-length(temp_data$mean_prediction)],temp_sd=rep(temp_data$sd_prediction,each=2)[-length(temp_data$sd_prediction)],ensemble=rep(temp_data$ensemble, each=2)[-length(temp_data$ensemble)])
 
 # Historical time-series fit -- linear model, HARV site
 
-# let's create a dataset that includes all of our variables that we can call it when fitting our model with JAGS
-# this first run we'll be using the EFI temperature data, which has hourly resolution, while the NEE data has half hourly resolution. For now, we'll match each half hour measurement to the corresponding hourly temperature
-# AT_half_hour <- list(parameter=rep(AT$parameter, each=2),datetime=as.POSIXlt(seq(from=as.POSIXct(AT$datetime[1]),to=as.POSIXct(AT$datetime[length(AT$datetime)]), by = "30 min")),variable=rep(AT$variable, each=2),prediction=rep(AT$prediction, each=2),site_id=rep(AT$site_id, each=2))
 # let's match up the dates from the NEON temp data and the nee observations
-temp_and_nee <- merge(temp,nee,by='datetime') |> filter(datetime>lubridate::date('2022-01-01'))
+temp_and_nee <- merge(temp_half_hour,nee,by='datetime')
+
+# the following was a way to filter the data by year, if needed
+# > filter(datetime>lubridate::date('2022-01-01'))
 # just_2022 <- dplyr::filter(temp_and_nee,format(datetime, "%Y") == "2022")
-data <- list(nee=temp_and_nee$observation,temp=temp_and_nee$temp,     ## data
+
+data <- list(datetime=temp_and_nee$datetime,nee=temp_and_nee$observation,temp=temp_and_nee$temp,     ## data
              nee_ic=temp_and_nee$observation[1],tau_ic=100, ## initial condition prior
              a_obs=1,r_obs=1,           ## obs error prior
              a_add=1,r_add=1            ## process error prior
@@ -135,6 +140,7 @@ write.csv(temp_and_nee, './Data/combined.csv')
 
 # now, let's use the ecoforecast package to run JAGS with a simple version of our model
 nee.out <- ecoforecastR::fit_dlm(model=list(obs="nee",fixed="~ 1 + X + temp",n.iter=2000),data)
+strsplit(nee.out$model,"\n",fixed = TRUE)[[1]] ## so we can verify what the JAGS code should look like
 params <- window(nee.out$params,start=1000) ## remove burn-in
 predicts <- window(nee.out$predict, start=1000)
 plot(params)
@@ -156,4 +162,4 @@ write.csv(out.params, './Data/sim_lin_params.csv')
 
 # a more complicated version:
 # model <- ecoforecastR::ParseFixed("1 + AT + exp(-k*LAI*SW))
-# where we predict nee and k?
+# where k,LAI,SW,AT forecasted?
